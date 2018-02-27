7116646b3fac0a336d886606a854bc84a6f37754
Erik <erikwjonsson@gmail.com>
2018-02-27 22:07:03 +0100

    - Changed name of spec and refactored it. - Deleted unused spec.

--- spec/lib/twitter_api_client_spec.rb
+++ spec/lib/twitter_api_client_spec.rb
@@ -4,7 +4,7 @@ describe TwitterApiClient do
     @api_client = TwitterApiClient.new
   end
 
-  context 'valid ticker and date' do
+  context 'valid username' do
     it 'receives valid json response' do
       response = @api_client.get('realdonaldtrump')
       expect(response.class).to eq(String)
@@ -15,12 +15,8 @@ describe TwitterApiClient do
   context 'invalid username' do
     it 'receives error message' do
       response = @api_client.get('SOMEINVALIDUSERNAME')
-      expect(response).to eq({"errors"=>[{"code"=>34, "message"=>"Sorry, that page does not exist."}]})
+      expect(response).to eq({"errors"=>[{"code"=>34,
+        "message"=>"Sorry, that page does not exist."}]})
     end
-  end
-
-  context 'invalid token' do
-     # NEED TO WRITE TEST HERE,
-    {"errors"=>[{"code"=>215, "message"=>"Bad Authentication data."}]}
   end
 end
